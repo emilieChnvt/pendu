2a0e2e3cdddcfc7f81826fb9e20c20c7a1c080bb
Emilie <emilie.chnvt@gmail.com>
2024-11-14 17:12:58 +0100

    motRanddomChoisi().includes(letterEnter.value)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
+const boutonEntrer = document.querySelector('.boutonEntrer');
+const letterEnter = document.getElementById('letterEnter');
+
+
+
 let mots = ["arraignee","bois", "table","chaise", "curseur", "chantier", "elephant", "fenetre"];
 let lettresAlphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 
 function motRanddomChoisi(){
     const motPendu = Math.floor(Math.random() * mots.length);
-
    return mots[motPendu];
+}
+console.log( motRanddomChoisi())
+
+
+if(motRanddomChoisi().includes(letterEnter.value)){
+    console.log(letterEnter.value)
 }
