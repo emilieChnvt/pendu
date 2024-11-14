f9f2843c7dd74263e1eaba14a184cb109126ef71
Emilie <emilie.chnvt@gmail.com>
2024-11-14 17:01:22 +0100

    random mots choosed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let mots = ["arraignee","bois", "table","chaise", "curseur", "chantier", "elephant", "fenetre"];
-
-
 let lettresAlphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+const motPendu = Math.floor(Math.random() * mots.length);
+
+console.log(mots[motPendu]);
