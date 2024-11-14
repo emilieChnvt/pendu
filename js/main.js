d6cd273f74db565d4d68ddcbd56d05bdbef649fe
Emilie <emilie.chnvt@gmail.com>
2024-11-14 17:04:24 +0100

    function motRandomChoisi added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ let mots = ["arraignee","bois", "table","chaise", "curseur", "chantier", "elepha
 let lettresAlphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 
-const motPendu = Math.floor(Math.random() * mots.length);
+function motRanddomChoisi(){
+    const motPendu = Math.floor(Math.random() * mots.length);
 
-console.log(mots[motPendu]);
+   return mots[motPendu];
+}
