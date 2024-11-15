e4c1f5e82330f5056428becb77bb9a6ff2f46bdf
Emilie <emilie.chnvt@gmail.com>
2024-11-15 15:22:53 +0100

    first and last letter added to the DOM

--- js/main.js
+++ js/main.js
@@ -9,35 +9,50 @@ const paragraphe = document.createElement('p')
 // Variables pour fonctions
 let mots = ["arraignee","bois", "table","chaise", "curseur", "chantier", "elephant", "fenetre"];
 let lettresAlphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let motCache = ['_']
+
 
 //EventListener
 boutonEntrer.addEventListener("click", findWordPendu);
 
-
-
 // Fonctions
-function motRanddomChoisi(){
-    const motPendu = Math.floor(Math.random() * mots.length);
 
+
+function getRandomWordFromDict(){
+    const motPendu = Math.floor(Math.random() * mots.length);
     return  mots[motPendu]
 }
-let motDecettePartie = motRanddomChoisi()
+let motDecettePartie = getRandomWordFromDict()
 console.log( motDecettePartie)
+motCache[0] = motDecettePartie[0]; // Première lettre
+motCache[motDecettePartie.length - 1] = motDecettePartie[motDecettePartie.length - 1]; // Dernière lettre
+
 
 
 function findWordPendu(){
     if(motDecettePartie.includes(letterEnter.value)){
+        for(let j = 0; j < motDecettePartie.length; j++){
+            if(motDecettePartie[j] === letterEnter.value){
+                motCache[j] = letterEnter.value;
+                console.log(motCache[j]);
+                response.innerHTML = motCache;
+            }
+        }
 
-        response.innerHTML +=  letterEnter.value;
     }
 }
 
 function sortFirstAndLastElement(){
-    let output = motDecettePartie[0] + motDecettePartie[motDecettePartie.length-1];
-    console.log(output);
+    //underscore
+        response.innerHTML += motCache;
+
+
 
 }
-sortFirstAndLastElement()
+sortFirstAndLastElement();
+
+
+
 
 
 
