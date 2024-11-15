671cb2fb42a614c0781c326ddf4645dac378df3b
Emilie <emilie.chnvt@gmail.com>
2024-11-15 18:25:32 +0100

    lettreEssayee added in anothe div

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@
 const boutonEntrer = document.querySelector('.boutonEntrer');
 const letterEnter = document.getElementById('letterEnter');
 const response = document.querySelector('.response');
+const containerLettres = document.querySelector('.lettresEssayees');
+
 
 // Variables pour le jeu
 let mots = ["arraignee", "bois", "table", "chaise", "curseur", "chantier", "elephant", "fenetre"];
 let motDecettePartie = getRandomWordFromDict(); // Mot aléatoire
 let motCache = Array(motDecettePartie.length).fill("_"); // Masquer le mot avec des underscores
+let lettreEssayee = [];
 
 // Initialiser la première et la dernière lettre
 initializeFirstAndLastLetter();
@@ -25,7 +28,7 @@ function getRandomWordFromDict() {
 function initializeFirstAndLastLetter() {
     motCache[0] = motDecettePartie[0];
     motCache[motDecettePartie.length - 1] = motDecettePartie[motDecettePartie.length - 1];
-    response.innerHTML = motCache.join(" ");
+    response.innerHTML = motCache.join(" "); // pour enlever les virgules
 }
 
     // traiter une tentative de lettre
@@ -38,12 +41,17 @@ function findWordPendu() {
         for (let i = 0; i < motDecettePartie.length; i++) {
             if (motDecettePartie[i] === lettre) {
                 motCache[i] = lettre; // Révéler la lettre correcte
+                lettreEssayee.push(lettre);
+                containerLettres.innerHTML = lettreEssayee
             }
+
         }
         response.innerHTML = motCache.join(" ");
     } else {
         alert("Mauvaise lettre ! Essayez encore.");
     }
+
+
 
     // Vérifier si le joueur a gagné
     if (motCache.join("") === motDecettePartie) {
